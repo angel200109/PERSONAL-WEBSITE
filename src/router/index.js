06ee4bb434a43fb094e0b672792389aee2011c16
Angel <2643152157@qq.com>
2025-01-07 23:18:19 +0800

    2025.1.7 23:18

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ import {
 import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     // {
     //   path: "/",
